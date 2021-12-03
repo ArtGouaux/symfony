4c8fd1d661e319e851d56f771943d9d76577fa95
Arthur GOUAUX <arthur.gouaux@gmail.com>
2021-12-03 11:22:32 +0100

    symfony quest 10 lol mdr

---
+++ src/Entity/Episode.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EpisodeRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=EpisodeRepository::class)
+ */
+class Episode
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Season::class, inversedBy="episodes")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $season_id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $number;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $synopsys;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSeasonId(): ?Season
+    {
+        return $this->season_id;
+    }
+
+    public function setSeasonId(int $season_id): self
+    {
+        $this->season_id = $season_id;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getNumber(): ?int
+    {
+        return $this->number;
+    }
+
+    public function setNumber(int $number): self
+    {
+        $this->number = $number;
+
+        return $this;
+    }
+
+    public function getSynopsys(): ?string
+    {
+        return $this->synopsys;
+    }
+
+    public function setSynopsys(?string $synopsys): self
+    {
+        $this->synopsys = $synopsys;
+
+        return $this;
+    }
+}
